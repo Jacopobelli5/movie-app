98bb762042def421ec57255e8057a054c6d6852d
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-29 22:10:12 +0100

    in home.jsx -> added search logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import MovieCard from '../components/MovieCard'
-import { useState } from 'react'
+import {useState} from 'react'
 
 export const Home = () => {
+    const [searchQuery, setSearchQuery] = useState("")
     const movies = [
         {id:1, title:"john wick", release_date:"2007"},
         {id:2, title:"john wick 2", release_date:"2010"},
@@ -10,19 +11,28 @@ export const Home = () => {
 
     ]
 
-    const handleSearch = () =>{
-
+    const handleSearch = (e) =>{
+        //preventing default behaviour so it wont reload the page
+        e.preventDefault();
+        setSearchQuery("")
     }
 
   return (
     <div className='home'>
         <form onSubmit={handleSearch} className='search-form'>
-            <input type="text" placeholder='search for movies' className='search-input'/>
+            <input 
+            type="text" 
+            placeholder='search for movies' 
+            className='search-input'
+            value={searchQuery}
+            onChange={(e) => {setSearchQuery(e.target.value)}}
+            />
             <button type='submit' className='search-button'>Search</button>
         </form>
+
         <div className='movies-grid'>
             {movies.map(movie => (
-                <MovieCard movie={movie} key={movie.id} /> //key property added
+               movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} /> //key property added
                 ))}
         </div>
     </div>
