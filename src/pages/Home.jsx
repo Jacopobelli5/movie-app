c60b5003d5acf5d6b13d642c6800b96319a79c09
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-30 22:03:06 +0100

    in Home.jsx -> implemented useEffect and search logic. In api.js -> fixed searchMovies function bug.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,49 @@
 import React from 'react'
 import MovieCard from '../components/MovieCard'
 import "../css/Home.css"
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import { getPopularMovies, searchMovies } from '../services/api'
 
 export const Home = () => {
     const [searchQuery, setSearchQuery] = useState("")
-    const movies = [
-        {id:1, title:"john wick", release_date:"2007"},
-        {id:2, title:"john wick 2", release_date:"2010"},
-        {id:3, title:"dune", release_date:"2020"}
+    const [movies, setMovies] = useState([])
+    // states to state the loading state and the possible error from api calls
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-    ]
-
-    const handleSearch = (e) =>{
+    // using useEffect to prevent default behaviour for api call function
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies)
+            } catch(err) {
+                console.log(err)
+                setError("Failed to load movies")
+            } finally{
+                setLoading(false)
+            }
+        }
+        loadPopularMovies();
+    }, [])
+    
+    const handleSearch = async (e) =>{
         //preventing default behaviour so it wont reload the page
         e.preventDefault();
-        setSearchQuery("")
+        if(!searchQuery.trim()){return}
+        if(loading) return
+
+        setLoading(true)
+        try {
+            const searchResult = await searchMovies(searchQuery)
+            setMovies(searchResult)
+            setError(null)
+        } catch (error) {
+            setError("Failed to search movies")
+        } finally{
+            setLoading(false)
+            setSearchQuery("")
+        }
     }
 
   return (
@@ -30,12 +58,15 @@ export const Home = () => {
             />
             <button type='submit' className='search-button'>Search</button>
         </form>
-
+        {/* error and loading rendering */}
+        {error && <div className='error-message'>{error}</div>}
+        {loading ? <div className='loading'>Loading...</div> :
         <div className='movies-grid'>
             {movies.map(movie => (
                movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} /> //key property added
                 ))}
         </div>
+        }
     </div>
   )
 }
