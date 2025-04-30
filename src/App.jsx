4095cb7e63a763429be82dd66c82df5e7809f1b4
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-30 20:45:28 +0100

    creted routes and added new component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Home } from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import { Favorites } from './pages/Favorites'
+import { NavBar } from './components/NavBar'
 
 
 function App() {
-return <Home/>
+return (
+    <div>
+        <NavBar />
+        <main className='main-content'>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/favorites" element={<Favorites />}/>
+            </Routes>
+        </main>
+    </div>
+)
 }
 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const NavBar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-brand'>
+            <Link to="/">Movie App</Link>
+        </div>
+        <div className='navbar-links'>
+            <Link to="/" className='nav-link'></Link>
+            <Link to="/favorites" className='nav-link'></Link>
+        </div>
+    </nav>
+  )
+}
